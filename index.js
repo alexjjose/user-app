af3c225cbde9f348c975a024bab51d726935ef4d
alexjjose <alexjjosea7@gmail.com>
2022-06-28 23:59:45 +0530

    mgu

---
+++ index.js
@@ -0,0 +1,60 @@
+indexbtnevent=()=>{
+    username=(document.getElementById("username").value)
+    password=(document.getElementById("password").value)
+
+
+
+
+ 
+    console.log(username)
+    alert(username)
+
+    console.log(password)
+    alert(password)
+
+}
+
+index1btnevent=()=>{
+
+
+    Name=(document.getElementById("name").value)
+    address=(document.getElementById("address").value)
+    mobileno=(document.getElementById("mobileno").value)
+    emailid=(document.getElementById("emailid").value)
+    bloodgroup=(document.getElementById("bloodgroup").value)
+    collage=(document.getElementById("collage").value)
+    username=(document.getElementById("username").value)
+    password=(document.getElementById("password").value)
+    conformpassword=(document.getElementById("confirmpassword").value)
+
+
+
+    console.log(Name)
+    alert(Name)
+
+    console.log(address)
+    alert(address)
+
+    console.log(mobileno)
+    alert(mobileno)
+
+    console.log(emailid)
+    alert(emailid)
+
+    console.log(bloodgroup)
+    alert(bloodgroup)
+
+    console.log(collage)
+    alert(collage)
+
+   
+    console.log(username)
+    alert(username)
+
+    console.log(password)
+    alert(password)
+    
+    console.log(conformpassword)
+    alert(conformpassword)
+
+}
